83693b48ce7bb3ba9abe074d7e0e0e9e85501ba0
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-28 12:52:50 +0530

    upadte new file Array

--- src/main/scala/Looping/ForLooping.scala
+++ src/main/scala/Looping/ForLooping.scala
@@ -3,10 +3,20 @@ package Looping
 object ForLooping {
   def main(args: Array[String]) : Unit ={
     val f= List("Apple","Banana","Mango")
-    for(d<- f)
+    for(d<- f) {
       println(d)
+
+    }
+
+    val number= Array(10,20,30,45)
+    for (n<-number){
+      println(n)
+    }
 
   }
 
 
 }
+
+
+
