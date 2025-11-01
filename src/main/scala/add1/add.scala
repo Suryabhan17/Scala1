79e44842ba54d08fc1a838c8813119ea85eafd83
Suryabhan17 <suryabhanuday99@gmail.com>
2025-11-01 21:12:13 +0530

    new add

--- src/main/scala/add1/add.scala
+++ src/main/scala/add1/add.scala
@@ -4,6 +4,8 @@ object add {
   def main(args: Array[String]): Unit = {
     val a =List (1, 2, 3, 4, 5, 6, 7)
   val b=a.filter(_>4)
-    println(b)
+   val c= for(n<-b) yield n+1
+
+    println(c)
   }
 }
