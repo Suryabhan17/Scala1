0b78f7f1192cabe90f031b0b84c44046fd9bbb0c
Suryabhan17 <suryabhanuday99@gmail.com>
2025-11-02 09:52:51 +0530

    new add groupBy and distinct and zip

--- src/main/scala/add1/add.scala
+++ src/main/scala/add1/add.scala
@@ -12,9 +12,40 @@ object add {
     val d=name ::: marks
     println(d)*/
 
-    val a=List(1,2,3,4,5,6)
+   /* val a=List(1,2,3,4,5,6)
     val square= a.map(x => x*2)
-    println(square)
+    println(square) */
+
+
+   /* val list=List(List(1,2,3,4,5,6), List(3,4))
+    val result =list.flatMap(x => x)
+    println(result) */
+
+  /*  val a=List(1,2,3,4,5,6)
+   val b= a.reduce (_ + _ )
+
+    println(b) */
+
+   /* val a= List("1","2","3","4","5","6")
+
+    val b=a.groupBy(_.charAt(0))
+    println(b) */
+
+   /* val a=List(1,2,3,4)
+    val b=List(5,6,7,8)
+    val c=a.zip(b)
+    println(c) */
+
+
+   /* val a=List(1,2,3,4,4,2,1,2,3)
+    val b=a.distinct
+    println(b) */
+
+
+
+
+
+
 
   }
 }
