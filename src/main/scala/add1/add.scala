fb9484f58bebd3ac5f7064e4d3eb2a5f05ec8e2c
Suryabhan17 <suryabhanuday99@gmail.com>
2025-12-04 17:38:17 +0530

    new

--- src/main/scala/add1/add.scala
+++ src/main/scala/add1/add.scala
@@ -56,7 +56,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
 
 
   def main(args: Array[String]): Unit = {
-  /*  val a =List (1, 2, 3, 4, 5, 6, 7)
+    /*  val a =List (1, 2, 3, 4, 5, 6, 7)
   val b=a.filter(_>4)
    val c= for(n<-b) yield n+1
 
@@ -66,32 +66,32 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     val d=name ::: marks
     println(d)*/
 
-   /* val a=List(1,2,3,4,5,6)
+    /* val a=List(1,2,3,4,5,6)
     val square= a.map(x => x*2)
     println(square) */
 
 
-   /* val list=List(List(1,2,3,4,5,6), List(3,4))
+    /* val list=List(List(1,2,3,4,5,6), List(3,4))
     val result =list.flatMap(x => x)
     println(result) */
 
-  /*  val a=List(1,2,3,4,5,6)
+    /*  val a=List(1,2,3,4,5,6)
    val b= a.reduce (_ + _ )
 
     println(b) */
 
-   /* val a= List("1","2","3","4","5","6")
+    /* val a= List("1","2","3","4","5","6")
 
     val b=a.groupBy(_.charAt(0))
     println(b) */
 
-   /* val a=List(1,2,3,4)
+    /* val a=List(1,2,3,4)
     val b=List(5,6,7,8)
     val c=a.zip(b)
     println(c) */
 
 
-   /* val a=List(1,2,3,4,4,2,1,2,3)
+    /* val a=List(1,2,3,4,4,2,1,2,3)
     val b=a.distinct
     println(b) */
 
@@ -99,49 +99,49 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     val b=a.map(x => x*x)
     println(b) */
 
-   /* val a= List(1,2,3,4,5,6,7,8,9,10)
+    /* val a= List(1,2,3,4,5,6,7,8,9,10)
     val b=a.reduce(_ + _)
     println(b) */
-   /* val a=List("Surya","1","Ajeet","2","Manish","3","Vishal","4","Sangam","5","Deepak","6","Anshuman","7")
+    /* val a=List("Surya","1","Ajeet","2","Manish","3","Vishal","4","Sangam","5","Deepak","6","Anshuman","7")
     val b=List("A","A","A","B","A","B","A")
     val c= a.groupBy(_.charAt(0))
     println(c) */
 
-   /* val a=List(1,2,3,4,5,1,23,4,4,5,2)
+    /* val a=List(1,2,3,4,5,1,23,4,4,5,2)
      println(a.distinct)
     */
 
-  /*  val a=List (1,2,3,4,5)
+    /*  val a=List (1,2,3,4,5)
     a.foreach( println)
    */
 
-   /* val a=Set(1,2,3,4,5)
+    /* val a=Set(1,2,3,4,5)
     val b=a.map(_*10) */
-   // println(b)
- // b.foreach(println)
- //  a.foreach(x=> println(x*10))
+    // println(b)
+    // b.foreach(println)
+    //  a.foreach(x=> println(x*10))
 
 
-/*  val a=Map("1"->"2","2"->"3","3"->"4")
+    /*  val a=Map("1"->"2","2"->"3","3"->"4")
     a.foreach { case (b,c) => println(s"$b and $c") }
  */
-  /*  val a=Map("Surya"->"A","Shyam"->"A","Vishal"->"A","Tinku"->"A")
+    /*  val a=Map("Surya"->"A","Shyam"->"A","Vishal"->"A","Tinku"->"A")
     a.foreach{case (c ,b) => println(s"$c => $b")}
 */
-   // val a= List(1,2,3,4,5,6,7)
-   // val b= for (n <- a if n%2==0 ){
+    // val a= List(1,2,3,4,5,6,7)
+    // val b= for (n <- a if n%2==0 ){
     //  println(s"$n is even number")
-  //  }
+    //  }
 
-   // a.foreach{n => if (n%2==0)
-  //    println(s"$n is even")
-  //  }
+    // a.foreach{n => if (n%2==0)
+    //    println(s"$n is even")
+    //  }
 
-  //  val fruits = List("banana", "peach", "lime", "pear", "cherry")
-  //  val newFruits=fruits.filter(f => f.length < 6 && f.startsWith("p"))
-   //   .map(_.capitalize)
-  //  println(newFruits)
- /* case class Student(name: String, marks: Int)
+    //  val fruits = List("banana", "peach", "lime", "pear", "cherry")
+    //  val newFruits=fruits.filter(f => f.length < 6 && f.startsWith("p"))
+    //   .map(_.capitalize)
+    //  println(newFruits)
+    /* case class Student(name: String, marks: Int)
 
     val students = List(
       Student("Amit", 85),
@@ -151,22 +151,22 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
 
     val sortedByMarks = students.sortBy(_.marks)
     println(sortedByMarks) */
-  // val a=ListBuffer(1,2,3,4,4,5,5,4)
-   // val b= List(1,2,3,4,5,6)
-   // val c=b.updated(1,50)
-  //  println(c)
+    // val a=ListBuffer(1,2,3,4,4,5,5,4)
+    // val b= List(1,2,3,4,5,6)
+    // val c=b.updated(1,50)
+    //  println(c)
     //val b=a.distinct
-   // a.insert(0,20)
+    // a.insert(0,20)
 
-  //  val c=a.filterNot(x=>  x==3 )
-  // val c=a.filterNot(x=> x==3 || x==1 || x!=4)
+    //  val c=a.filterNot(x=>  x==3 )
+    // val c=a.filterNot(x=> x==3 || x==1 || x!=4)
     //println(a)
-  //  println(b)
-   // println(c)
- // val a=List(1,2,3,4,5,6,7,8,9)
- //   val c=  a.map(x => x%2==0)
-  //  println(c)
-/*val a=List(1,2,3,4,5,6,7,8,9)
+    //  println(b)
+    // println(c)
+    // val a=List(1,2,3,4,5,6,7,8,9)
+    //   val c=  a.map(x => x%2==0)
+    //  println(c)
+    /*val a=List(1,2,3,4,5,6,7,8,9)
     val d=a.filter(_%2==0)
     val da=a.filter(_%2==1)
   val c=  a.map(x => x%2==0)
@@ -218,7 +218,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println("Main program continues...")
 
  */
-  /*  val a=List(1,2,3,4,5,6)
+    /*  val a=List(1,2,3,4,5,6)
   val ad=a.foldRight(2)(_+_)
     val(d,c)=a.splitAt(3)
     val as=a.grouped(2).toList
@@ -239,7 +239,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(c)
 
    */
-      /*
+    /*
     val a=List(1,2,3,4,5,5)
          val b= a.take(3)
        val d=  a.drop(3)
@@ -247,13 +247,13 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(b)
 
        */
-         //  val b=a.toString()
-      // println(s"${b+1}  add one element in this number ")
-     // val v=a.iterator
-     // val f=v.sum
-     //  println(f)
+    //  val b=a.toString()
+    // println(s"${b+1}  add one element in this number ")
+    // val v=a.iterator
+    // val f=v.sum
+    //  println(f)
 
-      /* val b=a.map(_+1)
+    /* val b=a.map(_+1)
        val c=for(n<-a ) yield n+1
         a.foreach(x => println(x+1))
       val d=a.filter(x => x % 2==0)
@@ -270,17 +270,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
      */
 
 
-
-
-
-
-
-
-
-
-
-
-   /* val a=Map("surya"->"1","surya1"->"2", "village"->"jhanga")
+    /* val a=Map("surya"->"1","surya1"->"2", "village"->"jhanga")
          //val b=a.values
  val c=a.filter{case (f, b) => b >=  "1" }
       // println(d)
@@ -289,21 +279,20 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(d) */
 
 
-
-     // println(b)
+    // println(b)
 
 
     // val a=Map("Math"->1,"Vishal"->5,"Village"->3)
-   // val b=a.map{ case (d,n)=>(d,n+1)}
+    // val b=a.map{ case (d,n)=>(d,n+1)}
     // println(b)
 
-   // println(a("Vishal"))
-   // for ((d,b)<- a)
+    // println(a("Vishal"))
+    // for ((d,b)<- a)
     //  println(s"$d and $b")
-   // println(a.get("Math"))
-   //
+    // println(a.get("Math"))
+    //
     // println((a.contains("surya")))
-   /*
+    /*
     val d=a.filter(_._2 > 3)
     println(d)
     val d1=a.filter{ case (v,k)=> k>2}
@@ -316,7 +305,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(expensive)
 
     */
-  /*
+    /*
     val a=List(1,1,1,1,1,1)
          val b=a.toIterator
            val c=b.sum
@@ -330,7 +319,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(c)
 
      */
-   /*
+    /*
     val nums = List(1, 1, 2, 3, 4, 5)
     val result = nums.take(3)
     println(result)
@@ -339,7 +328,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
         println(f)
 
     */
-   /*
+    /*
     val a=List(1,2,3,4,5,5,6)
     //val d=a.toIterable
     val g=a.sum
@@ -359,7 +348,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
 
 
      */
- /*
+    /*
     val a=Map("Surya"-> 45, "Surya1"->89,"Surya2"->89)
       a.map{case(d1,d2)=> println(s"Name=$d1 is roll no:$d2")}
     println()
@@ -368,8 +357,8 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
        println(a)
 
   */
-   // val a=List(1,2,3,4,5,6) // k=2,3
-     /*
+    // val a=List(1,2,3,4,5,6) // k=2,3
+    /*
     def A(n:List[Int], e:Int):Int={
       @tailrec
       def loop(x:List[Int],acc: Int=0):Int={
@@ -396,7 +385,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
     println(A(List(23,4,3,67,34)))
 
      */
-/*
+    /*
     val a=List(1,2,3,4,5)
       val d=a.fold(5)(_ + _)
     println(d)
@@ -451,7 +440,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
      */
 
 
-       /*
+    /*
     def Result() : Unit ={
       val t=new Thread (()=>{
         Thread.sleep(2000)
@@ -478,7 +467,7 @@ if (s.isEmpty) acc else countChar(s.tail, acc + 1)
 Result()
 
      */
-/*
+    /*
      var x=0
     val lock =new Object
     def g(): Unit =lock.synchronized{ x += 1}
@@ -493,7 +482,7 @@ Result()
 
 
  */
-   /*
+    /*
     val a= Future {20}
     val b= a andThen{
       case Success(v)=> println(v)
@@ -546,7 +535,7 @@ Result()
     println("Start")
 
      */
-/*
+    /*
    def Result()  ={
      val f=Future{
        Thread.sleep(500)
@@ -572,7 +561,7 @@ Result()
     demo()
 
      */
-     /*
+    /*
     class hello extends Actor {
       def receiver : Receiver ={
         case "hello"=> println("Surya")
@@ -636,14 +625,260 @@ Result()
 
 
      */
+
+    /*
     val a =45
     val b =45
   val c= for (i <- List(a) ; j <- List(b) ) yield (i + j)
 
     println(c)
+
+     */
+
+    /*
+   class Student private() {
+     def Ab(x: Int, y: Int): Unit = println(x + y)
+   }
+     object Student {
+       private val a = new Student()
+
+       def getb(): Student = a
+
+
+   }
+    val p=Student.getb()
+      p.Ab(45,89)
+
+        */
+    /*
+    class Person private() {
+      def Ab(name: String , age : Int): Unit=println(s"Name : $name and Age : $age")
+    }
+        object Person {
+          private val a= new Person()
+          def getb():Person=a
+        }
+    val p=Person.getb()
+      p.Ab("Ajit",56)
+
+
+     */
+    /*
+    case class A(name: String, age: Int)
+    val p = new A("Surya", 45)
+    p match {
+      case A(s, y) => println(s"Name : $s and Age : $y")
+    }
+    val a = List(p.age, p.name)
+    println(a)
+
+    val t = List(
+      A("Surya1", 45),
+      A("Surya2", 48),
+      A("Surya3", 42),
+      A("Surya4", 41)
+    )
+    t.foreach {
+      case A(name, age) => println(s"Name : $name and Age : $age")
+    }
+
+    println(p.hashCode())
+
+       */
+    /*
+    trait Student {
+      def a(x:Int , y:Int): Unit=println(x+y)
+
+    }
+       class Person extends Student{
+         def b() : Unit=println("New Number")
+       }
+    val f=new Person()
+     f.a(1,1)
+     f.b()
+
+      */
+    /*
+    trait Greeting {
+      def sayHello(): Unit=println("sayHello")
+    }
+    class Person extends Greeting {
+      def a():Unit=println("hello")
+    }
+    val p=new Person()
+      p.sayHello()
+    p.a()
+
+       */
+    /*
+    trait Info {
+      val role : String="Developer"
+      def showRole( ) : Unit=println(role)
+    }
+     class Employee extends  Info{
+       def a(x:Int , y:Int): Unit=println(s"Total Developer ${x+y}")
+     }
+    val p=new Employee()
+    p.showRole()
+    p.a(4,8)
+
+        */
+    /*
+    trait A {
+      def a():Unit=println("A")
+    }
+    trait B {
+      def b() : Unit=println("B")
+    }
+    class C extends A with B {
+      def c() : Unit=println("C")
+    }
+    val p=new C()
+    p.a()
+    p.b()
+    p.c()
+
+        */
+    /*
+    trait Animal {
+      def sound() : Unit=println("Sound")
+    }
+    class Dog extends Animal{
+      override def sound(): Unit = println("Dog Sound")
+    }
+    val p=new Dog()
+    p.sound()
+
+         */
+    /*
+    trait Shape {
+      def area() : Double
+      def printName(): Unit=println("Shape")
+    }
+    class Circle(r:Double) extends  Shape{
+      def area(): Double = 3.14 * r * r
+    }
+    val p=new Circle(8)
+    p.printName()
+   // p.area()
+    println(p.area())
+
+     */
+    /*
+    case class A(name:String, age:Int)
+      val a=A("Surya",12)
+     val b=a.copy(age= 45)
+   println(b)
+    println(a)
+
+       */
+    /*
+    class A(name:String) {
+      def a(): Unit=println("A")
+    }
+  case class B(name:String) extends A(name) {
+      def ab() : Unit=println("B")
+
+    }
+      val p=new B("Surya")
+      println(p)
+      p.a()
+     p.ab()
+
+    */
+
+    /*
+    class A private () {
+      object A {
+        private val a=new A()
+        def getb : A=a
+      }
+      val p=A.getb
+      val p1=A.getb
+      println(p eq p1)
+
+    */
+
+
+    
+      /*
+   class Student(val name:String, val age:Int) {
+      def this(name:String) = this(name,45)
+      def this()=this("Unknown",458)
+    }
+    val p=new Student("Surya",45)
+    val p1=new Student("Surya")
+    val p3=new Student()
+    println(p.name, p.age)
+    println(p.name)
+    println(p3.name)
+
+       */
+             /*
+    class Person(val name: String , val age:Int)
+       object Person {
+         def unapply(p: Person): Option[(String, Int)] =
+           Some((p.name, p.age))
+
+       }
+
+
+    val t=new Person("Surya",25)
+     t match {
+       case Person(x , y) => println(s"Name : $x and Age : $y")
+       case _=> println("Not match the condition")
+     }
+
+              */
+    /*
+         class A(val a:Int , val b:Int)
+           object A{
+         def unapply(p: A ) : Option[(Int , Int)]=
+           Some((p.a ,p.b))
+    }
+
+     */
+
+/*
+
+    class Surya(val name: String, val age: Int)
+    object Surya {
+      def unapply(p:Surya) : Option[(String , Int)]=
+        Some((p.name , p.age))
+    }
+      val t=new Surya("Ajeet", 45)
+      t match {
+        case Surya( x , y) if (y>18) => println(s"Name  : $x and Age : $y")
+        case _=> println("Not Match the Condition")
+      }
+
+ */
+         /*
+    case class A(name: String , age:Int)
+      val p=A("Surya", 45)
+       val  k=List(
+         A("Surya1",45),
+         A("Surya1",89),
+         A("Surya3", 48)
+       )
+      val Y=  k.foreach{ case A(x , y)  => println(s"$x and $y")}
+    val a=k.filter(_.age < 58)
+    println(a)
+
+          */
+       trait A {
+
+       def a( name:String):Unit=println(s"$name")
+       class B extends A {
+
+         def aa(age:Int) : Unit= println(age)
+       }
+     val p= new B()
+      println( p.a("Surya"))
+       p.aa(45)
+}
+
   }
-
-
 
 
 }
